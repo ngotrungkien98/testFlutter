0fe5b49b9882a4f10e69af4d11c2d0c753c6e4c3
TrungKien <ngotrungkien4798@gmail.com>
2020-07-15 14:30:48 +0700

    tai file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+echo "Session ID ".session_id()."<br/>" ;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H2>Demo Cookie</H2>
+    <!-- <form action="get_cookie.php" method="get">
+    User Name : <input type="text" name="uname"/> 
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+     -->
+     
+    </form>
+</body>
+</html>
